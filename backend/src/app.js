9798d36fdbf670a9b0fd70f089594c96eb2fefe3
nikesh-mgr <burjamagar2062@gmail.com>
2026-05-19 20:38:41 +0545

    namechanged

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 // ROUTES
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 app.use("/api/auth", authRoutes);
 
 export default app;
